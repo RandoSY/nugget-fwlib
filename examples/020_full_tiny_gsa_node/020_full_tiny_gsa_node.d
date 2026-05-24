1304b7c6667d4f848e354f86fd631e5d4b84ceae
RandoSY <RandoSY@users.noreply.github.com>
2026-05-24 16:23:22 -0400

    Verify direct include tiny GSA node compile

---
+++ examples/020_full_tiny_gsa_node/020_full_tiny_gsa_node.d
@@ -0,0 +1,2 @@
+C:\Users\rasyoung\Desktop\nugget-fwlib\examples\020_full_tiny_gsa_node\020_full_tiny_gsa_node.i: \
+  C:\Users\rasyoung\Desktop\nugget-fwlib\examples\020_full_tiny_gsa_node\020_full_tiny_gsa_node.S
